495a3ffd7a167eaecb9616a29096144eaa6d5702
joethorley <joe@poissonconsulting.ca>
2019-06-28 13:54:36 -0700

    is.natomic

--- R/is.R
+++ R/is.R
@@ -1,3 +1,20 @@
+#' Is natomic
+#' 
+#' Test whether x is a numeric, atomic object.
+#' An natomic object is a vector, matrix or array of typeof integer or double.
+#' 
+#' @param x The object to test.
+#'
+#' @return A flag indicating whether x is a natomic object.
+#' @export
+#'
+#' @examples
+#' is.natomic(list(x = 1))
+#' is.natomic(1)
+is.natomic <- function(x) {
+  is.atomic(x) && is.numeric(x)
+}
+
 #' Is nlist
 #' 
 #' Test whether x inherits from class \code{nlist} or \code{nlists}.
