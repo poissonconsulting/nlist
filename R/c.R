b9ef49bf87b93db36a4403ab5dac0d4f89b08993
joethorley <joe@poissonconsulting.ca>
2019-06-28 19:23:02 -0700

    add c

--- R/c.R
+++ R/c.R
@@ -10,3 +10,13 @@ c.nlist <- function(...) {
     err("nlist objects must have distinctly named natomic elements in order to be concatenated")
   x
 }
+
+#' @export
+c.nlists <- function(...) {
+  x <- list(...)
+  lapply(x, check_nlists, x_name  = "all objects in ...")
+  x <- lapply(x, unclass)
+  x <- do.call("c", x)
+  class(x) <- "nlists"
+  check_nlists(x, x_name = "the natomic elements of ...")
+}
--- tests/testthat/test-c.R
+++ tests/testthat/test-c.R
@@ -3,11 +3,24 @@ context("c")
 test_that("c.nlist", {
   expect_identical(c(nlist()), nlist())
   expect_identical(c(nlist(x = 1)), nlist(x = 1))
+  expect_error(c(nlist(x = 1), list(x = 1)), 
+               "all objects in ... must inherit from class nlist")
   expect_identical(c(nlist(x = 1), nlist(y = 2)), nlist(x = 1, y = 2))
   expect_identical(c(nlist(), nlist()), nlist())
   expect_identical(c(nlist(), nlist(x = 1)), nlist(x = 1))
   expect_error(c(nlist(x = 1), nlist(x = 1)), 
                "nlist objects must have distinctly named natomic elements in order to be concatenated")
-  expect_error(c(nlist(x = 1), list(x = 1)), 
-               "all objects in ... must inherit from class nlist")
+})
+
+test_that("c.nlists", {
+  expect_identical(c(nlists()), nlists())
+  expect_identical(c(nlists(nlist(x = 1))), nlists(nlist(x = 1)))
+  expect_identical(c(nlists(nlist(x = 1)),
+                     nlists(nlist(x = 1))), 
+                   nlists(nlist(x = 1), nlist(x = 1)))
+  expect_error(c(nlists(), list()), 
+               "all objects in ... must inherit from class nlists")
+  expect_error(c(nlists(nlist(x = 1)),
+                     nlists(nlist(y = 1))), 
+               "the names of the natomic elements of [.][.][.] must be identical")
 })
