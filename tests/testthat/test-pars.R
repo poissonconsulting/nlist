81f7eff2eb43ee35bd624a87cbddd9c8e184c202
joethorley <joe@poissonconsulting.ca>
2019-06-30 19:28:34 -0700

    test pars

--- tests/testthat/test-pars.R
+++ tests/testthat/test-pars.R
@@ -4,6 +4,9 @@ test_that("pars.nlist", {
   expect_identical(pars(nlist()), character(0))
   expect_identical(pars(nlist(x = 1)), "x")
   expect_identical(pars(nlist(x = 1, a = 1:10)), c("x", "a"))
+  expect_identical(pars(nlist(x = 1, a = 1:10), scalar_only = TRUE), "x")
+  expect_identical(pars(nlist(x = 1, a = 1:10), terms = TRUE), 
+                   c("x", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"))
 })
 
 test_that("pars.nlists", {
--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -13,8 +13,9 @@ test_that("subset.nlists",{
   expect_identical(subset(nlists()), nlists())
   expect_identical(subset(nlists(nlist())), nlists(nlist()))
   expect_identical(subset(nlists(nlist(x = 1))), nlists(nlist(x = 1)))
-  expect_identical(subset(nlist(x = 1, y = 2), pars  = "x"), nlist(x = 1))
-  expect_identical(subset(nlist(x = 1, y = 2), pars  = "y"), nlist(y = 2))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2)), pars  = "x"), nlists(nlist(x = 1)))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2)), pars  = "y"), nlists(nlist(y = 2)))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), pars  = "x"), nlists(nlist(x = 1), nlist(x = 3)))
   expect_error(subset(nlist(x = 1, y = 2), pars  = "z"), 
   "pars must be NULL OR pars can only include values 'x' or 'y'")
 })
