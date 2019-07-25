c37c7a8a98d339a6fb08abc57206b220d8f0b95b
joethorley <joe@poissonconsulting.ca>
2019-07-25 11:38:49 -0700

    add \n to print

--- R/print.R
+++ R/print.R
@@ -1,18 +1,18 @@
 #' @export
 print.nlist <- function(x, ...) {
   if(!length(x)) {
-    cat("an nlist object with 0 natomic elements")
+    cat("an nlist object with 0 natomic elements\n")
     return(invisible(x))
   }
   print(unclass(x))
-  cat(cn(length(x), "an nlist object with %n natomic element%s"))
+  cat(cn(length(x), "an nlist object with %n natomic element%s\n"))
   invisible(x)
 }
 
 #' @export
 print.nlists <- function(x, ...) {
   if(!length(x)) {
-    cat("an nlists object with 0 nlist objects")
+    cat("an nlists object with 0 nlist objects\n")
     return(invisible(x))
   }
   if(length(x[[1]]))
@@ -20,7 +20,7 @@ print.nlists <- function(x, ...) {
   str <- cn(length(x), 
             one = "an nlists object of an nlist object with",
             some = "an nlists object of %n nlist objects each with")
-  str <- p0(str, cn(length(x[[1]]), " %n natomic element%s"), collapse = "")
+  str <- p0(str, cn(length(x[[1]]), " %n natomic element%s\n"), collapse = "")
   cat(str)
   invisible(x)
 }
