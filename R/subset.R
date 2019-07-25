45fcbf27a507489f33deff0a1f46917de920d78a
joethorley <joe@poissonconsulting.ca>
2019-07-25 12:35:49 -0700

    subset pars to select and add subset to subset.nlists

--- R/subset.R
+++ R/subset.R
@@ -1,7 +1,7 @@
 #' Subset nlist
 #' 
 #' @param x An nlist object to be subsetted.
-#' @param pars A character vector of the parameters to include in the subsetted vector.
+#' @param select A character vector of the parameters to include in the subsetted object.
 #' @param ... Unused.
 #' @return The modified nlist object.
 #' @export
@@ -11,20 +11,21 @@
 #' subset(nlist)
 #' subset(nlist, "a")
 #' subset(nlist, c("x", "a"))
-subset.nlist <- function(x, pars = NULL, ...) {
-  checkor(check_null(pars), check_vector(pars, pars(x), unique = TRUE, only = TRUE))
+subset.nlist <- function(x, select = NULL, ...) {
+  if(is.null(select)) return(x)
+  
+  check_vector(select, pars(x), unique = TRUE, only = TRUE)
   check_unused(...)
   
-  if(is.null(pars)) return(x)
-  x[pars]
+  x[select]
 }
 
-#' Subset nlist
+#' Subset nlists
 #' 
-#' @param x An nlist object to be subsetted.
-#' @param pars A character vector of the parameters to include in the subsetted vector.
-#' @param ... Unused.
-#' @return The modified nlist object.
+#' @param x An nlists object to be subsetted.
+#' @inheritParams subset.nlist
+#' @param subset An integer vector of the nlist objects to include.
+#' @return The modified nlists object.
 #' @export
 #' 
 #' @examples 
@@ -32,15 +33,21 @@ subset.nlist <- function(x, pars = NULL, ...) {
 #'                 nlist(a = 2, y = 4, x = 4:1),
 #'                 nlist(a = 3, y = 6, x = 5:2))
 #' subset(nlists)
-#' subset(nlists, "a")
-#' subset(nlists, c("x", "a"))
-subset.nlists <- function(x, pars = NULL, ...) {
-  checkor(check_null(pars), check_vector(pars, pars(x), unique = TRUE, only = TRUE))
+#' subset(nlists, select = "a")
+#' subset(nlists, select = c("x", "a"))
+#' subset(nlists, subset = 1L)
+#' subset(nlists, subset = c(2L,2L))
+subset.nlists <- function(x, subset = NULL, select = NULL, ...) {
+  if(!is.null(subset)) {
+    check_vector(subset, 1L)
+    x <- x[subset]
+  }
+  if(is.null(select) || !length(x)) return(x)
+  
+  check_vector(select, pars(x), unique = TRUE, only = TRUE)
   check_unused(...)
   
-  if(is.null(pars)) return(x)
-  
-  x <- lapply(x, subset, pars = pars)
+  x <- lapply(x, subset, select = select)
   class(x) <- "nlists"
   x
 }
--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -3,21 +3,35 @@ context("subset")
 test_that("subset.nlist",{
   expect_identical(subset(nlist()), nlist())
   expect_identical(subset(nlist(x = 1)), nlist(x = 1))
-  expect_identical(subset(nlist(x = 1, y = 2), pars  = "x"), nlist(x = 1))
-  expect_identical(subset(nlist(x = 1, y = 2), pars  = "y"), nlist(y = 2))
-  expect_identical(subset(nlist(x = 1, y = 2), pars  = c("y", "x")), nlist(y = 2, x = 1))
-  expect_identical(subset(nlist(x = 1, y = 2), pars  = c("x", "y")), nlist(x = 1, y = 2))
-  expect_error(subset(nlist(x = 1, y = 2), pars  = "z"), 
-  "pars must be NULL OR pars can only include values 'x' or 'y'")
+  expect_identical(subset(nlist(x = 1, y = 2), select ="x"), nlist(x = 1))
+  expect_identical(subset(nlist(x = 1, y = 2), select ="y"), nlist(y = 2))
+  expect_identical(subset(nlist(x = 1, y = 2), select =c("y", "x")), nlist(y = 2, x = 1))
+  expect_identical(subset(nlist(x = 1, y = 2), select =c("x", "y")), nlist(x = 1, y = 2))
+  expect_error(subset(nlist(x = 1, y = 2), select ="z"), 
+               "select can only include values 'x' or 'y'")
 })
 
 test_that("subset.nlists",{
   expect_identical(subset(nlists()), nlists())
   expect_identical(subset(nlists(nlist())), nlists(nlist()))
   expect_identical(subset(nlists(nlist(x = 1))), nlists(nlist(x = 1)))
-  expect_identical(subset(nlists(nlist(x = 1, y = 2)), pars  = "x"), nlists(nlist(x = 1)))
-  expect_identical(subset(nlists(nlist(x = 1, y = 2)), pars  = "y"), nlists(nlist(y = 2)))
-  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), pars  = "x"), nlists(nlist(x = 1), nlist(x = 3)))
-  expect_error(subset(nlist(x = 1, y = 2), pars  = "z"), 
-  "pars must be NULL OR pars can only include values 'x' or 'y'")
+  expect_identical(subset(nlists(nlist(x = 1, y = 2)), select ="x"), nlists(nlist(x = 1)))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2)), select ="y"), nlists(nlist(y = 2)))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), select ="x"), nlists(nlist(x = 1), nlist(x = 3)))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), select = c("y", "x")), 
+                   subset(nlists(nlist(y = 2, x = 1), nlist(y = 2, x = 3))))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), 1L), 
+                   subset(nlists(nlist(x = 1, y = 2))))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), c(1L, 1L)), 
+                   subset(nlists(nlist(x = 1, y = 2), nlist(x = 1, y = 2))))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), c(2L, 1L)), 
+                   subset(nlists(nlist(x = 3, y = 2), nlist(x = 1, y = 2))))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), c(2L, 1L), "x"), 
+                   subset(nlists(nlist(x = 3), nlist(x = 1))))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), integer(0)), 
+                   subset(nlists()))
+  expect_identical(subset(nlists(nlist(x = 1, y = 2), nlist(x = 3, y = 2)), integer(0), "x"), 
+                   subset(nlists()))
+  expect_error(subset(nlist(x = 1, y = 2), select ="z"), 
+               "select can only include values 'x' or 'y'")
 })
