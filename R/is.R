c4f81f76e9f4b00b4d33c20292a13aee6eda9172
joethorley <joe@poissonconsulting.ca>
2020-01-03 16:24:37 -0800

    combine is

--- R/is.R
+++ R/is.R
@@ -1,29 +1,32 @@
-#' Is natomic
+#' Is natomic, nlist or nlists
 #'
-#' Test whether x is a [natomic_object()].
+#' Test whether x is a [natomic_object()], 
+#' [nlist_object()] or [nlists_object()].
 #'
 #' @param x The object to test.
 #'
-#' @return A flag indicating whether x is a natomic object.
+#' @return A flag indicating whether x is a natomic object (is atomic and numeric) or inherits from nlist or nlists.
+#' @name is_natomic
+NULL
+
+#' @describeIn is_natomic Is natomic
 #' @export
 #'
 #' @examples
+#' # is.natomic
 #' is.natomic(list(x = 1))
 #' is.natomic(1)
 is.natomic <- function(x) {
   is.atomic(x) && is.numeric(x)
 }
 
-#' Is nlist
-#'
-#' Test whether x is an [nlist_object()].
-#'
-#' @param x The object to test.
-#'
-#' @return A flag indicating whether x inherits from nlist.
+#' @describeIn is_natomic Is nlist
+#' 
 #' @export
 #'
 #' @examples
+#' 
+#' # is.nlist
 #' is.nlist(1)
 #' is.nlist(list(x = 1))
 #' is.nlist(nlist(x = 1))
@@ -31,16 +34,13 @@ is.nlist <- function(x) {
   inherits(x, "nlist")
 }
 
-#' Is nlists
+#' @describeIn is_natomic Is nlists
 #'
-#' Test whether x is an [nlists_object()].
-#'
-#' @param x The object to test.
-#'
-#' @return A flag indicating whether x inherits from nlists.
 #' @export
 #'
 #' @examples
+#' 
+#' # is.nlists
 #' is.nlists(nlist(x = 1))
 #' is.nlists(nlists(nlist(x = 2), nlist(x = 3.5)))
 is.nlists <- function(x) {
