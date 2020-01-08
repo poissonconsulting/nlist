d27c53cf9de76454f804274095a0f94a74c1b6cf
joethorley <joe@poissonconsulting.ca>
2020-01-08 12:24:20 -0800

    add unlist.nlist

---
+++ tests/testthat/test-unlist.R
@@ -0,0 +1,16 @@
+context("unlist")
+
+test_that("unlist.nlist", {
+  expect_identical(
+    unlist(nlist()),
+    structure(numeric(0), .Names = character(0))
+  )
+  expect_identical(
+    unlist(nlist(z = NA)),
+    c(z = NA_integer_)
+  )
+  expect_identical(
+    unlist(nlist(y = 2, x = matrix(4:7, ncol = 2))),
+    c(y = 2, `x[1,1]` = 4, `x[2,1]` = 5, `x[1,2]` = 6, `x[2,2]` = 7)
+  )
+})
