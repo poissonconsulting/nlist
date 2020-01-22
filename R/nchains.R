58d6171f9affe0759eb0646dbd67bf0eb2feebc7
joethorley <joe@poissonconsulting.ca>
2020-01-21 19:49:00 -0800

    nchains to universals

--- R/nchains.R
+++ R/nchains.R
@@ -1,28 +1,30 @@
-#' Number of MCMC chains
+#' @importFrom universals nchains
+#' @export
+universals::nchains
+
+#' Number of MCMC Chains of a nlist Object
 #'
-#' Gets the number of chains of an MCMC object.
+#' Gets the number of chains of an nlist object.
+#' 
+#' Always 1L.
 #'
-#' @param x An MCMC object
-#' @param ... Unused
-#' @return A count indicating the number of MCMC chains
+#' @inherit universals::nterms
 #' @export
 #'
 #' @examples
 #' nchains(nlist(x = 1:2))
-#' nchains(nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7))))
-#' nchains(split_chains(nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7)))))
-nchains <- function(x, ...) {
-  UseMethod("nchains")
-}
-
-#' @describeIn nchains Number of chains of an [nlist::nlist_object()]
-#'
-#' Always 1L.
-#' @export
 nchains.nlist <- function(x, ...) 1L
 
-#' @describeIn nchains Number of chains of an [nlist::nlists_object()]
+#' Number of MCMC Chains of a nlists Object
+#'
+#' Gets the number of chains of an nlists object.
+#'
+#' @inherit universals::nterms
 #' @export
+#' 
+#' @examples 
+#' nchains(nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7))))
+#' nchains(split_chains(nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7)))))
 nchains.nlists <- function(x, ...) {
   nchains <- attr(x, "nchains")
   if (is.null(nchains)) {
