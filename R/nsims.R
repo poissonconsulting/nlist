85360bbb8de6e461adda50f7603589deafa2d9bb
joethorley <joe@poissonconsulting.ca>
2020-01-23 11:37:43 -0800

    nsims to universals

--- R/nsims.R
+++ R/nsims.R
@@ -1,24 +1,27 @@
-#' Number of MCMC Simulations
-#'
-#' Gets the number of MCMC simulations (iterations * chains)
-#'
-#' @param x The object
-#' @param ... Unused.
-#' @return A count of the number of simulations.
-#' @seealso [niters()] and [nchains()]
+#' @importFrom universals nsims
 #' @export
+universals::nsims
+
+#' Number of MCMC Simulations
+#' 
+#' @details Always 1L.
+#' @inherit universals::nsims
+#' @export
+#'
 #' @examples
 #' nsims(nlist(x = 1:2))
+nsims.nlist <- function(x, ...) {
+  1L
+}
+
+#' Number of MCMC Simulations
+#' 
+#' @inherit universals::nsims
+#' @export
+#'
+#' @examples
 #' nsims(nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7))))
 #' nsims(split_chains(nlists(nlist(x = c(2, 9)), nlist(x = c(1, 7)))))
-nsims <- function(x, ...) UseMethod("nsims")
-
-#' @describeIn nsims Number of simulations of an nlist object
-#'
-#' Always 1L.
-#' @export
-nsims.nlist <- function(x, ...) 1L
-
-#' @describeIn nsims Number of simulations of an nlists object
-#' @export
-nsims.nlists <- function(x, ...) length(x)
+nsims.nlists <- function(x, ...) {
+  length(x)
+}
