756dac629d995ac297a8958fe313f2fde0551afc
joethorley <joe@poissonconsulting.ca>
2020-01-23 12:20:25 -0800

    collapse_chains to universals

--- R/collapse-chains.R
+++ R/collapse-chains.R
@@ -1,23 +1,23 @@
-#' Collapse Chains
-#'
-#' Collapses an MCMC object's chains into a single chain.
-#'
-#' @param x An MCMC object.
-#' @param ... Unused.
+#' @importFrom universals collapse_chains
 #' @export
-collapse_chains <- function(x, ...) {
-  UseMethod("collapse_chains")
-}
+universals::collapse_chains
 
-#' @describeIn collapse_chains Collapse chains for an nlist object
-#'
+#' @inherit universals::collapse_chains
+#' @details 
 #' As nlist objects can only have 1 chain the object is unchanged.
 #' @export
+#'
+#' @examples
+#' collapse_chains(nlist(x = 2))
 collapse_chains.nlist <- function(x, ...) {
   x
 }
 
-#' @describeIn collapse_chains Collapse chains for an nlists object
+#' @inherit universals::collapse_chains
+#' @export
+#'
+#' @examples
+#' collapse_chains(nlist(x = 2))
 #' @export
 collapse_chains.nlists <- function(x, ...) {
   attr(x, "nchains") <- NULL
