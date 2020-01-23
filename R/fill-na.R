fc1786dd8c9ca40654991ebab18bb33a0029d0b8
joethorley <joe@poissonconsulting.ca>
2020-01-23 12:42:54 -0800

    fill_na for atomic etc to extras

--- R/fill-na.R
+++ R/fill-na.R
@@ -1,59 +1,12 @@
-#' Fill Missing Values
-#'
-#' Fills missing values.
-#'
-#' @param x The object.
-#' @param value A scalar of the value to replace missing values with.
-#' @param ... Unused.
-#' @return The object with missing values filled.
+#' @importFrom universals fill_na
+#' @export
+universals::fill_na
+
+#' @inherit universals::fill_na
 #' @export
 #' @examples
 #' fill_na(nlist(x = c(2, NA), y = matrix(c(1:3, NA), nrow = 2)))
 #' fill_na(nlists(nlist(x = c(2, NA)), nlist(x = c(NA_real_, NA))))
-fill_na <- function(x, ...) UseMethod("fill_na")
-
-#' @describeIn fill_na Fill missing values of logical object
-#' @export
-fill_na.logical <- function(x, value = FALSE, ...) {
-  chk_scalar(value)
-  chk_unused(...)
-  value <- as.logical(value)
-  x[is.na(x)] <- value
-  x
-}
-
-#' @describeIn fill_na Fill missing values of integer object
-#' @export
-fill_na.integer <- function(x, value = 0L, ...) {
-  chk_scalar(value)
-  chk_unused(...)
-  value <- as.integer(value)
-  x[is.na(x)] <- value
-  x
-}
-
-#' @describeIn fill_na Fill missing values of numeric object
-#' @export
-fill_na.numeric <- function(x, value = 0, ...) {
-  chk_scalar(value)
-  chk_unused(...)
-  value <- as.numeric(value)
-  x[is.na(x)] <- value
-  x
-}
-
-#' @describeIn fill_na Fill missing values of character object
-#' @export
-fill_na.character <- function(x, value = "0", ...) {
-  chk_scalar(value)
-  chk_unused(...)
-  value <- as.character(value)
-  x[is.na(x)] <- value
-  x
-}
-
-#' @describeIn fill_na Fill missing values of nlist object
-#' @export
 fill_na.nlist <- function(x, value = 0L, ...) {
   chk_scalar(value)
   chk_unused(...)
@@ -62,8 +15,10 @@ fill_na.nlist <- function(x, value = 0L, ...) {
   x
 }
 
-#' @describeIn fill_na Fill missing values of nlist object
+#' @inherit universals::fill_na
 #' @export
+#' @examples
+#' fill_na(nlist(x = c(2, NA), y = matrix(c(1:3, NA), nrow = 2)))
 fill_na.nlists <- function(x, value = 0L, ...) {
   chk_unused(...)
   x <- lapply(x, fill_na, value = value)
--- tests/testthat/test-fill-na.R
+++ tests/testthat/test-fill-na.R
@@ -1,41 +1,5 @@
 context("fill-na")
 
-test_that("fill_na atomic objects", {
-  expect_identical(fill_na(numeric(0)), numeric(0))
-  expect_identical(fill_na(integer(0)), integer(0))
-  expect_identical(fill_na(NA), FALSE)
-  expect_identical(fill_na(c(10L, NA)), c(10L, 0L))
-  expect_identical(
-    fill_na(matrix(c(1L, 3L, 7L, NA), nrow = 2)),
-    matrix(c(1L, 3L, 7L, 0L), nrow = 2)
-  )
-  expect_identical(fill_na(c(10L, NA), value = 11L), c(10L, 11L))
-  expect_identical(fill_na(c(10L, NA), value = 11), c(10L, 11L))
-  expect_identical(
-    fill_na(matrix(c(TRUE, NA, FALSE, NA), nrow = 2),
-      value = "TRUE"
-    ),
-    matrix(c(TRUE, TRUE, FALSE, TRUE), nrow = 2)
-  )
-})
-
-test_that("fill_na.nlist", {
-  expect_identical(
-    fill_na(nlist()),
-    nlist()
-  )
-  expect_identical(
-    fill_na(nlist(x = c(2, NA), y = matrix(c(1:3, NA), nrow = 2))),
-    nlist(x = c(2, 0), y = matrix(c(1:3, 0L), nrow = 2))
-  )
-  expect_identical(
-    fill_na(nlist(x = c(2, NA), y = matrix(c(1:3, NA), nrow = 2)),
-      value = 5
-    ),
-    nlist(x = c(2, 5), y = matrix(c(1:3, 5L), nrow = 2))
-  )
-})
-
 test_that("fill_na.nlists", {
   expect_identical(
     fill_na(nlists()),
