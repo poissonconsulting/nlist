c7a8155dc37958a1be2f72bae95583f65b9534db
joethorley <joe@poissonconsulting.ca>
2020-01-23 14:06:43 -0800

    nlist-object to nlist

--- R/nlist.R
+++ R/nlist.R
@@ -1,8 +1,15 @@
 #' Create nlist Object
 #'
 #' Creates a [nlist_object()] from one of more uniquely named [natomic_object()] arguments.
+#' 
+#' An nlist object is an S3 class list of uniquely named
+#' [natomic_object()] elements.
+#'
+#' nlist objects are the raw data inputs for analytic engines
+#' such as JAGS, STAN and TMB.
 #' @param ... Uniquely named atomic numeric objects.
 #' @return An nlist object.
+#' @aliases nlist-object nlist_object
 #' @export
 #'
 #' @examples
