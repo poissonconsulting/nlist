f9553814539ced8884e457287b21849830237be4
joethorley <joe@poissonconsulting.ca>
2020-02-02 16:04:16 -0800

    added as.data.frame.nlists

--- R/as-data-frame.R
+++ R/as-data-frame.R
@@ -1,3 +1,29 @@
+.as.data.frame.nlist <- function(x, sample = NULL) {
+  if(!length(x)) {
+    if(!is.null(sample)) {
+      return(as.data.frame(tibble::tibble(
+        term = term(0), 
+        sample = integer(0),
+        value = numeric(0))))
+    }
+    return(as.data.frame(tibble::tibble(
+      term = term(0), 
+      value = numeric(0))))
+  }
+  x <- unlist(x)
+  term <- as.term(names(x))
+  x <- unname(x)
+  if(!is.null(sample)) {
+    return(as.data.frame(tibble::tibble(
+      term = term, 
+      sample = sample, 
+      value = x)))
+  } 
+  as.data.frame(tibble::tibble(
+    term = term, 
+    value = x))
+}
+
 #' Coerce nlist Object to Data Frame
 #' 
 #' Coerces an nlist object to a data.frame with an term column and a value column.
@@ -10,11 +36,29 @@
 #' as.data.frame(nlist(x = 1, y = 4:6))
 as.data.frame.nlist <- function(x, ...) {
   chk_unused(...)
-  if(!length(x)) 
-    return(as.data.frame(tibble::tibble(term = term(0), value = numeric(0))))
-  x <- unlist(x)
-  term <- as.term(names(x))
-  x <- unname(x)
-  x <- as.data.frame(tibble::tibble(term = term, value = x))
+  .as.data.frame.nlist(x)
+}
+
+#' Coerce nlists Object to Data Frame
+#' 
+#' Coerces an nlists object to a data.frame with a term, sample and value column.
+#' 
+#' @export
+#' @param x An nlists object.
+#' @inheritParams params
+#' @return A data.frame.
+#' @examples 
+#' as.data.frame(nlists(nlist(x = 1, y = 4:6), 
+#'   nlist(x = 3, y = 1:3)))
+as.data.frame.nlists <- function(x, ...) {
+  chk_unused(...)
+  if(!length(x) || !length(x[[1]])) 
+    return(as.data.frame(tibble::tibble(term = term(0), 
+                                        sample = integer(0),
+                                        value = numeric(0))))
+  x <- mapply(.as.data.frame.nlist, x, sample = 1:length(x),
+              SIMPLIFY = FALSE)
+  x <- do.call("rbind", x)
   x
 }
+
--- tests/testthat/test-as-data-frame.R
+++ tests/testthat/test-as-data-frame.R
@@ -8,3 +8,22 @@ test_that("as.data.frame.nlist", {
         expect_identical(as.data.frame(nlist(x = 1, y = 1:2)), structure(list(term = structure(c("x", "y[1]", "y[2]"), class = c("term", 
 "character")), value = c(1, 1, 2)), row.names = c(NA, -3L), class = "data.frame"))
 })
+
+test_that("as.data.frame.nlists", {
+  expect_identical(as.data.frame(nlists()), structure(list(term = structure(character(0), class = c("term", 
+"character")), sample = integer(0), value = numeric(0)), row.names = integer(0), class = "data.frame"))
+  expect_identical(as.data.frame(nlists(nlist())), structure(list(term = structure(character(0), class = c("term", 
+"character")), sample = integer(0), value = numeric(0)), row.names = integer(0), class = "data.frame"))
+  expect_identical(as.data.frame(nlists(nlist(x = 2))), structure(list(term = structure("x", class = c("term", "character"
+)), sample = 1L, value = 2), row.names = c(NA, -1L), class = "data.frame"))
+    expect_identical(as.data.frame(nlists(nlist(x = 2:4))), structure(list(term = structure(c("x[1]", "x[2]", "x[3]"), class = c("term", 
+"character")), sample = c(1L, 1L, 1L), value = c(2, 3, 4)), row.names = c(NA, 
+-3L), class = "data.frame"))
+        expect_identical(as.data.frame(nlists(nlist(y = 1, s = 1:2))), structure(list(term = structure(c("y", "s[1]", "s[2]"), class = c("term", 
+"character")), sample = c(1L, 1L, 1L), value = c(1, 1, 2)), row.names = c(NA, 
+-3L), class = "data.frame"))
+    expect_equal(as.data.frame(nlists(nlist(x = 1, y = 1:2), nlist(x = 1, y = 3:4))), structure(list(term = structure(c("x", "y[1]", "y[2]", "x", "y[1]", 
+"y[2]"), class = c("term", "character")), sample = c(1L, 1L, 
+1L, 2L, 2L, 2L), value = c(1, 1, 2, 1, 3, 4)), row.names = c(NA, 
+-6L), class = "data.frame"))
+})
