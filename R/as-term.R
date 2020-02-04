64e03147c3edbf7c5e3f7784c115b7909eeba540
joethorley <joe@poissonconsulting.ca>
2020-02-04 11:37:26 -0800

    fix warning

--- R/as-term.R
+++ R/as-term.R
@@ -9,7 +9,7 @@ as.term.nlist <- function(x, ...) {
   x <- mapply(as.term, x, names(x), SIMPLIFY = FALSE)
   x <- unname(unlist(x))
   if (is.null(x)) {
-    return(term(0L))
+    return(term(x = 0))
   }
   as.term(x)
 }
