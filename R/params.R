3de65505955b6300dd695dee59b148852eecfac7
joethorley <joe@poissonconsulting.ca>
2020-03-31 13:25:03 -0700

    params man

--- R/params.R
+++ R/params.R
@@ -6,7 +6,7 @@
 #' @param flesh An atomic vector
 #' @param skeleton An nlist object.
 #' @param scalar A logical scalar specifying whether to include
-#' all parameters (NA), only scalars (TRUE) or all parameters
+#' all parameters (NULL), only scalars (TRUE) or all parameters
 #' except scalars (FALSE).
 #' @param terms A flag specifying whether to return the parameter name
 #' for each term element.
