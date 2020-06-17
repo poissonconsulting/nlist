9ffeef43e68ae52f249c2b340ff318e26365e87f
Joe Thorley <joe@poissonconsulting.ca>
2020-06-17 14:44:49 -0700

    - Fix tests ready for term 0.2.0.

--- tests/testthat/test-as-term.R
+++ tests/testthat/test-as-term.R
@@ -3,50 +3,44 @@ context("as-term")
 test_that("as.term.nlist", {
   expect_identical(
     term::as.term(nlist()),
-    structure(character(0), class = c("term", "character"))
+    as.term(character(0))
   )
   expect_identical(
     term::as.term(nlist(x = 1)),
-    structure("x", class = c("term", "character"))
+    as.term("x")
   )
   expect_identical(
     term::as.term(nlist(x = 1:2)),
-    structure(c("x[1]", "x[2]"), class = c("term", "character"))
+    as.term(c("x[1]", "x[2]"))
   )
   expect_identical(
     term::as.term(nlist(x = 1, y = 2:3)),
-    structure(c("x", "y[1]", "y[2]"), class = c("term", "character"))
+    as.term(c("x", "y[1]", "y[2]"))
   )
   expect_identical(
     term::as.term(nlist(x = 1, y = matrix(1:3, c(1, 3)))),
-    structure(c("x", "y[1,1]", "y[1,2]", "y[1,3]"), class = c(
-      "term",
-      "character"
-    ))
+    as.term(c("x", "y[1,1]", "y[1,2]", "y[1,3]"))
   )
 })
 
 test_that("as.term.nlists", {
   expect_identical(
     term::as.term(nlists(nlist())),
-    structure(character(0), class = c("term", "character"))
+    as.term(character(0))
   )
   expect_identical(
     term::as.term(nlists(nlist(x = 1))),
-    structure("x", class = c("term", "character"))
+    as.term("x")
   )
   expect_identical(
     term::as.term(nlists(nlist(x = 1:2))),
-    structure(c("x[1]", "x[2]"), class = c("term", "character"))
+    as.term(c("x[1]", "x[2]"))
   )
   expect_identical(
     term::as.term(nlists(
       nlist(x = 1, y = matrix(1:3, c(1, 3))),
       nlist(x = 1, y = matrix(1:3, c(1, 3)))
     )),
-    structure(c("x", "y[1,1]", "y[1,2]", "y[1,3]"), class = c(
-      "term",
-      "character"
-    ))
+    as.term(c("x", "y[1,1]", "y[1,2]", "y[1,3]"))
   )
 })
