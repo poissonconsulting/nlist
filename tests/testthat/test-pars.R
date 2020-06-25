97030847e3b4eb9cf42ed758dce3accb3dc310c3
Joe Thorley <joe@poissonconsulting.ca>
2020-06-25 10:04:54 -0700

    additional tests

--- tests/testthat/test-pars.R
+++ tests/testthat/test-pars.R
@@ -2,15 +2,15 @@ test_that("pars.nlist", {
   expect_identical(pars(nlist()), character(0))
   expect_identical(pars(nlist(x = 1)), "x")
   expect_identical(pars(nlist(x = 1, a = 1:10)), c("x", "a"))
-
+  
   expect_identical(pars(nlist(x = 1, a = 1:10), scalar = TRUE), "x")
   expect_identical(pars(nlist(x = 1, a = 1:10), scalar = TRUE), "x")
   lifecycle::expect_deprecated(pars(nlist(x = 1, a = 1:10), terms = TRUE))
   rlang::scoped_options(lifecycle_verbosity = "quiet")
   expect_identical(
-      pars(nlist(x = 1, a = 1:10), terms = TRUE),
-      c("x", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a")
-    )
+    pars(nlist(x = 1, a = 1:10), terms = TRUE),
+    c("x", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a")
+  )
 })
 
 test_that("pars.nlists", {
@@ -39,4 +39,11 @@ test_that("pars.nlists scalar", {
   expect_identical(pars(nlists(nlist(x = 1, a = 1:10)), scalar = TRUE), "x")
   expect_identical(pars(nlists(nlist(a = 1:10)), scalar = TRUE), character(0))
   expect_identical(pars(nlists(nlist(x = 1, a = 1:10), nlist(x = 2, a = 1:10)), scalar = TRUE), "x")
+})
+
+test_that("pars.nlists term", {
+  rlang::scoped_options(lifecycle_verbosity = "quiet")
+  lifecycle::expect_deprecated(pars(nlists(nlist(x = 1, a = 1:2)), terms = TRUE))
+  
+  expect_identical(pars(nlists(nlist(x = 1, a = 1:2)), terms = TRUE), c("x", "a", "a"))
 })
