8977dae60d650981cd902885ca5d669d78817acf
Joe Thorley <joe@poissonconsulting.ca>
2025-01-03 15:58:24 -0800

    update pkgdown to template

--- R/nlist-package.R
+++ R/nlist-package.R
@@ -1,7 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import chk generics universals extras term
 #' @import lifecycle
 #' @importFrom coda as.mcmc as.mcmc.list thin
 #' @importFrom stats aggregate median
 #' @importFrom abind abind
 #' @importFrom purrr transpose
+## usethis namespace: end
 NULL
